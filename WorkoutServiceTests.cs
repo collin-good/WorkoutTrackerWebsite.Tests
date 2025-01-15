a463ef4717e77c59a7d2ac56a7ec9a9999441062
Collin Good <152931501+collin-good@users.noreply.github.com>
2025-01-15 18:23:37 -0500

    Completed the get all and get tests, created  add, update, and delete tests

--- WorkoutServiceTests.cs
+++ WorkoutServiceTests.cs
@@ -1,11 +1,51 @@
+using WorkoutTrackerWebsite.Data;
+using WorkoutTrackerWebsite.Models;
+using WorkoutTrackerWebsite.Services;
+
 namespace WorkoutTrackerWebsite.Tests;
 
 [TestClass]
 public class WorkoutServiceTests
 {
+    WorkoutTestContext context = new();
+
     [TestMethod]
-    public void TestMethod1()
+    public void ServiceGetAllTest()
+    {
+        WorkoutService service = new WorkoutService(context);
+
+        List<Workout> workouts = service.GetSortedWorkouts();
+
+        Assert.IsNotNull(workouts);
+        Assert.AreEqual<List<Workout>>(context._testDB, workouts);
+    }
+
+    [TestMethod]
+    public void ServiceGetTest()
+    {
+        WorkoutService service = new(context);
+
+        Workout? workout = service.Get(0);
+
+        Assert.IsNotNull(workout);
+        Assert.AreEqual<Workout>(context._testDB, workout);
+    }
+    
+    [TestMethod]
+    public void ServiceAddTest()
     {
 
+    }
+
+    [TestMethod]
+    public void ServiceUpdateTest()
+    {
+
+    }
+
+    [TestMethod]
+    public void ServiceDeleteTest()
+    {
+        
     }
 }
